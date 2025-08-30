a671405d0a76d191b79bdba681ae4c031af5c84e
Samael-p <samm22mc@gmail.com>
2025-08-30 06:54:37 -0400

    newsVerifier

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { Header } from '../layout/header/header';
-
+import { NewsVerifier } from '../pages/news-verifier/news-verifier';
 
 @Component({
   selector: 'app-home',
-  imports: [Header],
+  imports: [Header, NewsVerifier],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
